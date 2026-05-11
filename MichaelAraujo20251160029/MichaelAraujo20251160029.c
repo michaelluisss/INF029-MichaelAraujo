34237506ca9ae54b8b41723a4d447e057d1e85d5
Michael <ldmichael838@gmail.com>
2026-05-11 00:37:32 -0300

    Implementar Q1 - Validação de Data
    - Implementa função q1 para validar datas no formato dd/mm/aaaa
    - Suporta dias e meses com um ou dois dígitos
    - Suporta anos com dois ou quatro dígitos
    - Valida meses com 31, 30 e 28/29 dias (incluindo anos bissextos)
    - Sem uso de funções de manipulação de strings (strtok)

---
+++ MichaelAraujo20251160029/MichaelAraujo20251160029.c
@@ -0,0 +1,383 @@
+// #################################################
+//  Instituto Federal da Bahia
+//  Salvador - BA
+//  Curso de Análise e Desenvolvimento de Sistemas http://ads.ifba.edu.br
+//  Disciplina: INF029 - Laboratório de Programação
+//  Professor: Renato Novais - renato@ifba.edu.br
+
+//  ----- Orientações gerais -----
+//  Descrição: esse arquivo deve conter as questões do trabalho do aluno.
+//  O aluno deve preencher seus dados abaixo, e implementar as questões do trabalho
+
+//  ----- Dados do Aluno -----
+//  Nome:Michael Luis de Jesus Araujo
+//  email:ldmichael838@gmail.com
+//  Matrícula:20251160029
+//  Semestre:2026.2
+
+//  Copyright © 2016 Renato Novais. All rights reserved.
+// Última atualização: 07/05/2021 - 19/08/2016 - 17/10/2025
+
+// #################################################
+
+#include <stdio.h>
+#include "MichaelAraujo20251160029.h" 
+#include <stdlib.h>
+
+
+DataQuebrada quebraData(char data[]);
+
+/*
+## função utilizada para testes  ##
+
+ somar = somar dois valores
+@objetivo
+    Somar dois valores x e y e retonar o resultado da soma
+@entrada
+    dois inteiros x e y
+@saida
+    resultado da soma (x + y)
+ */
+int somar(int x, int y)
+{
+    int soma;
+    soma = x + y;
+    return soma;
+}
+
+/*
+## função utilizada para testes  ##
+
+ fatorial = fatorial de um número
+@objetivo
+    calcular o fatorial de um número
+@entrada
+    um inteiro x
+@saida
+    fatorial de x -> x!
+ */
+int fatorial(int x)
+{ //função utilizada para testes
+  int i, fat = 1;
+    
+  for (i = x; i > 1; i--)
+    fat = fat * i;
+    
+  return fat;
+}
+
+int teste(int a)
+{
+    int val;
+    if (a == 2)
+        val = 3;
+    else
+        val = 4;
+
+    return val;
+}
+
+/*
+ Q1 = validar data
+@objetivo
+    Validar uma data
+@entrada
+    uma string data. Formatos que devem ser aceitos: dd/mm/aaaa, onde dd = dia, mm = mês, e aaaa, igual ao ano. dd em mm podem ter apenas um digito, e aaaa podem ter apenas dois digitos.
+@saida
+    0 -> se data inválida
+    1 -> se data válida
+ @restrições
+    Não utilizar funções próprias de string (ex: strtok)   
+    pode utilizar strlen para pegar o tamanho da string
+ */
+ int q1(char data[])
+ {
+    int contabarra = 0;
+    int i = 0;
+    int sDia = 0;
+    int sMes = 0;
+    int sAno = 0;
+    //quebrar a string data em strings sDia, sMes, sAno
+    while(data[i] != '\0'){
+        if(data[i] == '/'){
+            contabarra++;
+        }else{
+             if(data[i + 1] ==  '/' || data[i + 1] == '\0' ){
+                if(contabarra == 0){
+                    
+                    sDia += ( data[i] - 48);
+    
+                }else  if(contabarra == 1){
+                    
+                    sMes += ( data[i] - 48);
+    
+                }else  if(contabarra == 2){ 
+                    
+                    sAno += ( data[i] - 48);
+                }
+    
+             }else{
+                if(contabarra == 0){
+                    sDia += ( data[i] - 48) * 10;
+    
+                }else  if(contabarra == 1){
+                    sMes += ( data[i] - 48) * 10;
+    
+                }else  if(contabarra == 2 ){
+                    sAno += ( data[i] - 48) * 10;
+                    if(data[i + 1] != '\0' && data[i + 2] != '\0'){
+                        sAno*= 10;
+                    }
+    
+                }
+             }
+        }
+    i++;
+    }
+    printf("%d\n%d\n%d\n",sDia,sMes,sAno);
+    if((sAno > 0 && sAno <= 99) ||
+        (sAno >999 && sAno <=9999) ){
+
+
+
+
+
+     
+            
+                if((sMes == 1 || sMes == 3 || sMes == 5 || sMes == 7 ||
+                    sMes == 8 || sMes == 10 || sMes == 12)&&
+                    sDia >= 1 && sDia <= 31){return 1;}
+            
+                if((sMes == 4 || sMes == 6 || sMes == 9 || sMes == 11)&&
+                    sDia >= 1 && sDia <= 30){ return 1;}
+            
+            
+                if(sMes == 2){
+                    if(((sAno % 4 == 0 && sAno % 100 != 0) || sAno % 400 == 0 )){
+
+                        if(sDia>=1 && sDia <= 29){return 1;}
+                        else{
+                            return 0;
+                        }
+            
+                    }else{if(sDia >= 1 && sDia <= 28){return 1;}else{return 0; }
+                    }
+                
+               
+                }
+                else{
+                    return 0;
+                }
+            
+            
+
+    }else{    
+        return 0;
+    }        
+
+}
+/*
+ Q2 = diferença entre duas datas
+ @objetivo
+    Calcular a diferença em anos, meses e dias entre duas datas
+ @entrada
+    uma string datainicial, uma string datafinal. 
+ @saida
+    Retorna um tipo DiasMesesAnos. No atributo retorno, deve ter os possíveis valores abaixo
+    1 -> cálculo de diferença realizado com sucesso
+    2 -> datainicial inválida
+    3 -> datafinal inválida
+    4 -> datainicial > datafinal
+    Caso o cálculo esteja correto, os atributos qtdDias, qtdMeses e qtdAnos devem ser preenchidos com os valores correspondentes.
+ 
+DiasMesesAnos q2(char datainicial[], char datafinal[])
+{
+
+    //calcule os dados e armazene nas três variáveis a seguir
+    DiasMesesAnos dma;
+
+    if (q1(datainicial) == 0){
+      dma.retorno = 2;
+      return dma;
+    }else if (q1(datafinal) == 0){
+      dma.retorno = 3;
+      return dma;
+    }else{
+      //verifique se a data final não é menor que a data inicial
+      
+      //calcule a distancia entre as datas
+
+
+      //se tudo der certo
+      dma.retorno = 1;
+      return dma;
+      
+    }
+    
+}
+*/
+/*
+ Q3 = encontrar caracter em texto
+ @objetivo
+    Pesquisar quantas vezes um determinado caracter ocorre em um texto
+ @entrada
+    uma string texto, um caracter c e um inteiro que informa se é uma pesquisa Case Sensitive ou não. Se isCaseSensitive = 1, a pesquisa deve considerar diferenças entre maiúsculos e minúsculos.
+        Se isCaseSensitive != 1, a pesquisa não deve  considerar diferenças entre maiúsculos e minúsculos.
+ @saida
+    Um número n >= 0.
+ 
+int q3(char *texto, char c, int isCaseSensitive)
+{
+    int qtdOcorrencias = -1;
+
+    return qtdOcorrencias;
+}
+
+
+ Q4 = encontrar palavra em texto
+ @objetivo
+    Pesquisar todas as ocorrências de uma palavra em um texto
+ @entrada
+    uma string texto base (strTexto), uma string strBusca e um vetor de inteiros (posicoes) que irá guardar as posições de início e fim de cada ocorrência da palavra (strBusca) no texto base (texto).
+ @saida
+    Um número n >= 0 correspondente a quantidade de ocorrências encontradas.
+    O vetor posicoes deve ser preenchido com cada entrada e saída correspondente. Por exemplo, se tiver uma única ocorrência, a posição 0 do vetor deve ser preenchido com o índice de início do texto, e na posição 1, deve ser preenchido com o índice de fim da ocorrencias. Se tiver duas ocorrências, a segunda ocorrência será amazenado nas posições 2 e 3, e assim consecutivamente. Suponha a string "Instituto Federal da Bahia", e palavra de busca "dera". Como há uma ocorrência da palavra de busca no texto, deve-se armazenar no vetor, da seguinte forma:
+        posicoes[0] = 13;
+        posicoes[1] = 16;
+        Observe que o índice da posição no texto deve começar ser contado a partir de 1.
+        O retorno da função, n, nesse caso seria 1;
+
+ 
+int q4(char *strTexto, char *strBusca, int posicoes[30])
+{
+    int qtdOcorrencias = -1;
+
+    return qtdOcorrencias;
+}
+
+
+ Q5 = inverte número
+ @objetivo
+    Inverter número inteiro
+ @entrada
+    uma int num.
+ @saida
+    Número invertido
+*/
+int q5(int num)
+{
+    
+    int i = 0;
+    int mil = 0;
+    int cen = 0;
+    int dez = 0;
+    int uni = 0;
+    int r= 0;
+    if(num > 0){
+        if(num >99){
+            if(num > 999){
+                mil = num / 1000;
+                r = num % 1000;
+            }
+            cen = r / 100;
+            r = r % 100;
+        }
+        dez = r / 10;
+        uni = r;
+    }
+    num = (uni * 1000)+(dez * 100)+(cen * 10)+ mil;
+    return num;
+}
+/*
+
+ Q6 = ocorrência de um número em outro
+ @objetivo
+    Verificar quantidade de vezes da ocorrência de um número em outro
+ @entrada
+    Um número base (numerobase) e um número de busca (numerobusca).
+ @saida
+    Quantidade de vezes que número de busca ocorre em número base
+
+
+int q6(int numerobase, int numerobusca)
+{
+    int qtdOcorrencias;
+    return qtdOcorrencias;
+}
+
+
+ Q7 = jogo busca palavras
+ @objetivo
+    Verificar se existe uma string em uma matriz de caracteres em todas as direções e sentidos possíves
+ @entrada
+    Uma matriz de caracteres e uma string de busca (palavra).
+ @saida
+    1 se achou 0 se não achou
+
+
+ int q7(char matriz[8][10], char palavra[5])
+ {
+     int achou;
+     return achou;
+ }
+
+
+
+DataQuebrada quebraData(char data[]){
+  DataQuebrada dq;
+  char sDia[3];
+	char sMes[3];
+	char sAno[5];
+	int i; 
+
+	for (i = 0; data[i] != '/'; i++){
+		sDia[i] = data[i];	
+	}
+	if(i == 1 || i == 2){ // testa se tem 1 ou dois digitos
+		sDia[i] = '\0';  // coloca o barra zero no final
+	}else {
+		dq.valido = 0;
+    return dq;
+  }  
+	
+
+	int j = i + 1; //anda 1 cada para pular a barra
+	i = 0;
+
+	for (; data[j] != '/'; j++){
+		sMes[i] = data[j];
+		i++;
+	}
+
+	if(i == 1 || i == 2){ // testa se tem 1 ou dois digitos
+		sMes[i] = '\0';  // coloca o barra zero no final
+	}else {
+		dq.valido = 0;
+    return dq;
+  }
+	
+
+	j = j + 1; //anda 1 cada para pular a barra
+	i = 0;
+	
+	for(; data[j] != '\0'; j++){
+	 	sAno[i] = data[j];
+	 	i++;
+	}
+
+	if(i == 2 || i == 4){ // testa se tem 2 ou 4 digitos
+		sAno[i] = '\0';  // coloca o barra zero no final
+	}else {
+		dq.valido = 0;
+    return dq;
+  }
+
+  dq.iDia = atoi(sDia);
+  dq.iMes = atoi(sMes);
+  dq.iAno = atoi(sAno); 
+
+	dq.valido = 1;
+    
+  return dq;
+}*/
+
